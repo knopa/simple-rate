31b3e513c564d3ab53998b0634019114744f734a
Maksym Klishevych <imaksimus@gmail.com>
2017-12-15 18:10:28 +0200

    solution

---
+++ mklishevych.js
@@ -0,0 +1,13 @@
+function my(a, b, c, x) {
+    var ratio = x / (a + b + c);
+    var y = a * ratio;
+    var v = b * ratio;
+    var z = x - (y + v);
+
+    return { y: y, v: v, z: z, sum: (y + v + z) }
+}
+
+// tests
+console.log(my(2, 138, 11, 40));
+console.log(my(2, 13, 11, 40));
+console.log(my(22, 22, 22, 40));
